a4cf4ebe859d7f54a85332ddac959515e6959b4f
Jorge Garcia <jorgeg@nvidia.com>
2019-03-08 13:08:40 -0800

    [R4DS] Add more exercise samples from ch 03

--- R4DS/ch03/firstSteps.r
+++ R4DS/ch03/firstSteps.r
@@ -3,3 +3,17 @@ library(tidyverse)
 # Create a simple graph with the mpg dataset
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy))
+# Map categorical value to color class
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = class))
+# Map categorical variable to continuos parameter
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
+# Use more correct aestetics to do the same
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
+# Use custom change that make more beauty, but no extra info
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
